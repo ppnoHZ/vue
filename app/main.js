08188cf37e117b8af055262f821cd538a4dd3e60
zhoudd <zhoudd@litsoft.com.cn>
2018-10-10 17:41:52 +0800

    Calendar App

--- app/main.js
+++ app/main.js
@@ -1,21 +1,68 @@
+const submissionComponent = {
+    template: `
+    <div  style="display: flex; width: 100%">
+        <figure class="media-left">
+        <img class="image is-64x64" v-bind:src="submission.submissionImage">
+        </figure>
+        <div class="media-content">
+            <div class="content">
+                <p>
+                    <strong>
+                        <a v-bind:href="submission.url" class="has-text-info">{{submission.title}}</a>
+                        <span class="tag is-small">#{{submission.id}}</span>
+                    </strong>
+                    <br>
+                    {{submission.description}}
+                    <br>
+                    <small class="is-size-7">
+                        Submitted by:
+                        <img class="image is-24x24" :src="submission.avatar">
+                    </small>
+                </p>
+            </div>
+        </div>
+        <div class="media-right">
+            <span class="icon is-small" @click="upvote(submission.id)">
+                <i class="fa fa-chevron-up"></i>
+                <strong class="has-text-info">{{submission.votes}}</strong>
+            </span>
+        </div>
+    </div>`,
+    props:[
+        'submission','submissions'
+    ],
+    methods:{
+        upvote(submissionId) {
+            // const submission = this.submissions.find(
+            //     submission => submission.id === submissionId
+            // );
+            this.submission.votes++;
+        }
+    }
+}
+
+
+
+// Vue.component('submission-component', {
+
+// })
 new Vue({
     el: "#app",
     data: {
         submissions: Seed.submissions
     },
+    components: {
+        'submission-component':submissionComponent
+    },
     computed: {
         sortedSubmissions() {
+            console.log("computed:sortedSubmissions");
             return this.submissions.sort((a, b) => {
                 return b.votes - a.votes
             });
         }
     },
     methods: {
-        upvote(submissionId) {
-            const submission = this.submissions.find(
-                submission => submission.id === submissionId
-            );
-            submission.votes++;
-        }
+       
     }
 })
---
+++ calendar/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+   <CalendarWeek />
+   <CalendarEntry />
+  </div>
+</template>
+
+<script>
+import CalendarEntry from "./components/CalendarEntry.vue";
+import CalendarWeek from "./components/CalendarWeek.vue";
+export default {
+  name: "App",
+  components: {
+    CalendarEntry,
+    CalendarWeek
+  }
+};
+</script>
+
+<style lang="scss">
+html, body {
+  height: 100%;
+}
+</style>
+
+<style lang="scss" scoped>
+#app {
+  height: inherit;
+  background: #6e6e6e;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  -webkit-align-items: center;
+  justify-content: center;
+  -webkit-justify-content: center;
+}
+</style>
