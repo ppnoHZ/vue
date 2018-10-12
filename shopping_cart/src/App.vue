4994ebadec4af67d539fc8c10e9b96e78373a858
zhoudd <zhoudd@litsoft.com.cn>
2018-10-12 17:50:16 +0800

    shopping cart

---
+++ shopping_cart/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="columns">
+        <div class="column is-3">
+          <CartList />
+        </div>
+        <div class="column is-9">
+          <ProductList />
+        </div>
+      </div>
+   </div>
+  </div>
+</template>
+
+<script>
+import CartList from "./components/cart/CartList.vue";
+import ProductList from "./components/product/ProductList.vue";
+
+export default {
+  name: "app",
+  components: { CartList, ProductList }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ shopping_cart/src/store/modules/product/index.js
@@ -0,0 +1,36 @@
+import Axios from "axios";
+
+const state = {
+    productItems: []
+};
+
+const mutations = {
+
+    UPDATE_PRODUCT_ITEMS(state, payload) {
+        state.productItems = payload;
+    }
+};
+
+const actions = {
+    getProductItems({ commit }) {
+        Axios.get('/api/products').then((respone) => {
+            console.log(respone);
+            commit('UPDATE_PRODUCT_ITEMS', respone.data);
+        })
+    }
+};
+
+const getters = {
+    productItems(state) {
+        return state.productItems;
+    }
+};
+
+const productModule = {
+    state,
+    mutations,
+    actions,
+    getters
+}
+
+export default productModule;
